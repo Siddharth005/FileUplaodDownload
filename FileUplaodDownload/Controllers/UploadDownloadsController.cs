2a76ab9abcd2efe93434327758cabf2e43d56ce4
Siddharth005 <54567859+Siddharth005@users.noreply.github.com>
2020-10-30 17:54:57 +0530

    Add project files.

---
+++ FileUplaodDownload/Controllers/UploadDownloadsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using FileUplaodDownload.Models;
+
+namespace FileUplaodDownload.Controllers
+{
+    public class UploadDownloadsController : Controller
+    {
+        private FileUploadDownloadEntities db = new FileUploadDownloadEntities();
+
+        // GET: UploadDownloads
+        public ActionResult Index()
+        {
+            return View(db.UploadDownloads.ToList());
+        }
+
+        // GET: UploadDownloads/Details/5
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UploadDownload uploadDownload = db.UploadDownloads.Find(id);
+
+            if (uploadDownload == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uploadDownload);
+        }
+
+        public FileResult Download(int? id)
+        {
+
+            UploadDownload uploadDownload = db.UploadDownloads.Find(id);
+
+            return File(uploadDownload.filedata, uploadDownload.filetype, uploadDownload.filename);
+        }
+
+
+
+        // GET: UploadDownloads/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UploadDownloads/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "fileId,filename,filetype,filedata,ticketnumber")] UploadDownload uploadDownload, HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                if (upload != null && upload.ContentLength > 0)
+                {
+
+                    uploadDownload.filename = System.IO.Path.GetFileName(upload.FileName);
+                    uploadDownload.filetype = upload.ContentType;
+                    
+
+                };
+                using (var reader = new System.IO.BinaryReader(upload.InputStream))
+                {
+                    uploadDownload.filedata = reader.ReadBytes(upload.ContentLength);
+                }
+
+            }
+            db.UploadDownloads.Add(uploadDownload);
+            db.SaveChanges();
+
+            return RedirectToAction("Index"); ;
+        }
+
+        // GET: UploadDownloads/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UploadDownload uploadDownload = db.UploadDownloads.Find(id);
+            if (uploadDownload == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uploadDownload);
+        }
+
+        // POST: UploadDownloads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UploadDownload uploadDownload = db.UploadDownloads.Find(id);
+            db.UploadDownloads.Remove(uploadDownload);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FileUplaodDownload/Controllers/employeeloginsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FileUplaodDownload.Models;
+
+namespace FileUplaodDownload.Controllers
+{
+    public class employeeloginsController : Controller
+    {
+        private FileUploadDownloadEntities db = new FileUploadDownloadEntities();
+
+        // GET: employeelogins/login
+        public ActionResult login()
+        {
+            return View();
+        }
+        // GET: employeelogins/Details/5
+        public ActionResult logins(employeelogin employeelogin2)
+        {
+
+            employeelogin employeelogin1 = (employeelogin)(from e in db.employeelogins
+                                                           where e.pass == employeelogin2.pass
+                                                           select e).First();
+            if (employeelogin2 == null)
+            {
+                return HttpNotFound();
+            }
+            return RedirectToAction("Index","UploadDownloads");
+        }
+
+        // GET: employeelogins/Create
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
